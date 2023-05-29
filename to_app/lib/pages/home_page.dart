29d48849ff044d641611fa436a896fd0679d8587
remydev47 <brianrimidev47@gmail.com>
2023-05-29 20:01:19 +0300

    app completion

--- to_app/lib/pages/home_page.dart
+++ to_app/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_app/utils/DialogBox.dart';
 import 'package:to_app/utils/todo_tile.dart';
 
 class Homepage extends StatefulWidget {
@@ -9,6 +10,44 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  //text controller
+  final _controller = TextEditingController();
+
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["WorkOut", false],
+    ["Meditation", false],
+    ["Implement App", false]
+  ];
+  //checkBox was tapped
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+    Navigator.of(context).pop();
+  }
+
+  //save task
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+    });
+  }
+
+  //create New task
+  void createNewTask() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return DialogBox(
+            controller: _controller,
+            onSave: saveNewTask,
+            onCancel: () => Navigator.of(context).pop(),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +56,19 @@ class _HomepageState extends State<Homepage> {
         title: Text('To Do'),
         elevation: 0,
       ),
-      body: ListView(
-        children: [ToDoTile()],
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
     );
   }
--- to_app/lib/utils/todo_tile.dart
+++ to_app/lib/utils/todo_tile.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  ToDoTile(
+      {super.key,
+      required this.taskName,
+      required this.taskCompleted,
+      required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Make Tutorials"),
-      decoration: BoxDecoration(color: Colors.yellow),
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25),
+      child: Slidable(
+        // endActionPanel: ActionPanel(),
+        child: Container(
+          padding: EdgeInsets.all(24.0),
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black,
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                    decoration: taskCompleted
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
